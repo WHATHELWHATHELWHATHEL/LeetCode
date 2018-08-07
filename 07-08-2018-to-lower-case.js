a3fb4594a97205b4bab7901c876e244b38d061cd
逸凡 <yifan.chen@wormpex.com>
2018-08-07 22:16:28 +0800

    convert the to lower case

---
+++ 07-08-2018-to-lower-case.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+
+// lower case lower bound a => 97
+// lower case upper bound z => 122
+// upper case lower bound A => 65
+// upper case upper bound Z => 90
+
+const isLowerCase = char => (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122);
+const isUpperCase = char => (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90);
+
+const toLowerCase = str => (
+  str.split('')
+  // convert the upper case
+  .map(char => isUpperCase(char) ? (
+    String.fromCharCode(char.charCodeAt(0) + 32)
+  ) : (
+    char
+  ))
+  .join('')
+);
+
+// Test Ground
+// console.log(toLowerCase('Hello'));
+// console.log(toLowerCase('here'));
+// console.log(toLowerCase('LOVELY'));
+// console.log(toLowerCase("al&phaBET"));
