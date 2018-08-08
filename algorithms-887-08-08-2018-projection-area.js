fcc6f6debded0694e5b75f602cc59f73486f515a
逸凡 <yifan.chen@wormpex.com>
2018-08-08 10:47:04 +0800

    change the name of files to futher understand the staff

---
+++ algorithms-887-08-08-2018-projection-area.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+// This Method parse the raw cube info into meaningful data
+const parseValue = () => {};
+
+const projectionArea = function(grid) {
+
+};
