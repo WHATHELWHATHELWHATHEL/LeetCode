5f3d117d6f331ad0730da68d6ff6c18ebb6af782
逸凡 <yifan.chen@wormpex.com>
2018-10-22 14:01:05 +0800

    add a problem into TODO status

---
+++ TODO-1540043872417-algorithms-806-20-10-2018-number-of-line-to-write-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} widths
+ * @param {string} S
+ * @return {number[]}
+ */
+
+// ??? can understand what it is talking about
+
+const numberOfLines = (widths, S) => { // eslint-disable-line
+
+};
