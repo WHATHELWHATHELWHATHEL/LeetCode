9e1bce349cbc1d25f3be22ff1809d7df5b87f6a2
逸凡 <yifan.chen@wormpex.com>
2018-10-26 10:39:51 +0800

    save problematic code here

---
+++ problematic-1540470232964-algorithms-292-25-10-2018-nim-game.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+const canWinNim = (n) => { // eslint-disable-lines
+  // take all the remain, win
+  if (n === 1 || n === 2 || n === 3) {
+    return true;
+  }
+  // 
+};
