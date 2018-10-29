fb74009346856ba79210a90b348a3ff7ab5bd1af
逸凡 <yifan.chen@wormpex.com>
2018-10-29 21:07:54 +0800

    finish max consecutive one

--- 1540795666715-algorithms-485-29-10-2018-max-consecutive-one.js
+++ 1540795666715-algorithms-485-29-10-2018-max-consecutive-one.js
@@ -3,33 +3,32 @@
  * @return {number}
  */
 
-const findMaxConsecutiveOnes = (nums) => { // eslint-disable-line
-  const processList = nums.reduce(
-    (accu, value) => {
-      if (value === 0) {
-        accu.push({ value, count: -10 });
-        return accu;
+const findMaxConsecutiveOnes = (nums) => ( // eslint-disable-line
+  nums.reduce(
+    (accu, digit) => {
+      if (accu.length === 0) {
+        if (digit === 0) {
+          accu.push({ digit, count: 0 });
+        } else {
+          accu.push({ digit, count: 1 });
+        }
+      } else {
+        if (digit === 0) {
+          accu.push({ digit, count: 0 });
+        } else {
+          const lastElement = accu[accu.length - 1];
+          if (lastElement.digit === digit) {
+            accu.push({ digit, count: lastElement.count + 1 });
+          } else {
+            accu.push({ digit, count: 1 });
+          }
+        }
       }
-      if (accu.length <= 1) {
-        accu.push({ value, count: 1 });
-        return accu;
-      }
-      const lastElement = accu[accu.length - 1];
-      if (lastElement.value === value) {
-        accu.push({ value, count: lastElement.count + 1 });
-        return accu;
-      }
-      accu.push({ value, count: 1 });
       return accu;
     },
     [],
-  );
-  let largestCount = -1;
-  const length = processList.length;
-  for (let index = 0; index < length; index += 1) {
-    if (processList[index].count > largestCount) {
-      largestCount = processList[index].count;
-    }
-  }
-  return largestCount;
-};
+  ).reduce(
+    (largest, { count }) => (count > largest ? count : largest),
+    0,
+  )
+);
