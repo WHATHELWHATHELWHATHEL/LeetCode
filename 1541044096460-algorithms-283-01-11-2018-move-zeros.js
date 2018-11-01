e0d953c5aabc5e3a0ebc5aa6b4cd5b609cd247f4
逸凡 <yifan.chen@wormpex.com>
2018-11-01 12:20:56 +0800

    save the real way here

--- 1541044096460-algorithms-283-01-11-2018-move-zeros.js
+++ 1541044096460-algorithms-283-01-11-2018-move-zeros.js
@@ -4,14 +4,33 @@
  */
 
 // tricky way
+// const moveZeroes = (nums) => { // eslint-disable-line
+//   const numsLength = nums.length;
+//   const filteredList = nums.filter(value => value !== 0);
+//   const lengthDiff = numsLength - filteredList.length;
+//   for (let index = 0; index < lengthDiff; index += 1) {
+//     filteredList.push(0);
+//   }
+//   for (let index = 0; index < numsLength; index += 1) {
+//     nums[index] = filteredList[index];
+//   }
+// };
+
+// real way
+
 const moveZeroes = (nums) => { // eslint-disable-line
-  const numsLength = nums.length;
-  const filteredList = nums.filter(value => value !== 0);
-  const lengthDiff = numsLength - filteredList.length;
-  for (let index = 0; index < lengthDiff; index += 1) {
-    filteredList.push(0);
+  const numberCount = nums.length;
+  const zeroCount = nums.reduce((sum, value) => (value === 0 ? sum + 1 : sum), 0);
+
+  for (let index = 0; index < numberCount; index += 1) {
+    if (nums[index] !== 0) {
+      nums.push(nums[index]);
+    }
   }
-  for (let index = 0; index < numsLength; index += 1) {
-    nums[index] = filteredList[index];
+  for (let index = 0; index < numberCount; index += 1) {
+    nums.shift();
+  }
+  for (let index = 0; index < zeroCount; index += 1) {
+    nums.push(0);
   }
 };
